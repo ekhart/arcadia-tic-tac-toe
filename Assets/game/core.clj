c5856ee6bbc55be49ff2856ee10352ac2219bb45
Artur "ekhart" Dębkowski <ekhtrash@gmail.com>
2018-01-31 01:23:24 +0100

    add clojure 1.9 dependecny, use spec, def basic specs

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -1,21 +1,43 @@
 (ns game.core
-	(require [clojure.string :as str])
-	(use arcadia.core)
+	(require [clojure.string :as str]
+		[clojure.spec :as s])
+	(use arcadia.core
+		clojure.repl)
 	(import [UnityEngine 
 		GameObject
 		Camera]
 		[UnityEngine.UI Button Text]))
 
+; https://clojure.org/guides/spec
+; [clojure.spec.alpha :as s]) ; dont work
+; [clojure.spec :as s]) ; this work
+
+; clojure.repl for doc fun, but dont work ;/
+; https://stackoverflow.com/questions/8332611/how-to-use-clojure-doc-function
+
+(s/def ::game-object? #(= (type %) UnityEngine.GameObject))
+(doc ::game-object?)
 
 (if-let [exist (object-named "Manager")]
 	(def manager exist)
 	(def manager (GameObject. "Manager")))
 
+(s/valid? ::game-object? manager) ; false?
+(inst? manager) ; false too
+(= (type manager) UnityEngine.GameObject) ; true
+
+
 (defn get-array []
 	(vec (map vec (to-array-2d (repeat 3 (repeat 3 nil))))))
 
+; (s/def ::manager-state (s/keys :req [::array ::current-player]))
+; use unqualified keys
+(s/def ::manager-state (s/keys :req-un [::array ::current-player]))
+
 (state+ manager :array (get-array))
 (state+ manager :current-player :X)
+
+(s/valid? ::manager-state (state manager))
 
 (defn update-array 
 	([array] (update-array array 0 0 1))
@@ -27,7 +49,7 @@
 
 (defn set-current-player []
 	(update-state manager :current-player #(if (= % :X) :O :X))
-	(with-cmpt (object-named "Current Player") [text-cmpt Text]
+	(with-cmpt (object-nam	ed "Current Player") [text-cmpt Text]
 		(set! (. text-cmpt text) 
 			(str "Current Player: " (name (state manager :current-player))))))
 
